3d2a5b3aba33eb9cc2e9dafe5afbb33f06e88618
Mreibs <ebbesen_2@hotmail.com>
2025-04-06 14:32:29 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LysSim from "./LysSim";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LysSim />
+  </React.StrictMode>
+);
